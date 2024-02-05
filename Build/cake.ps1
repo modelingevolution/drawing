9e4db14522da4a9ff656d2c8045d18f78ace95c7
Rafał Maciąg <rafal.maciag@modelingevolution.com>
2024-02-05 22:02:08 +0100

    delete

--- Build/cake.ps1
+++
@@ -1,7 +0,0 @@
-New-Cake -Name "ModelingEvolution.Drawing" -Root "../Sources/ModelingEvolution.Drawing"
-
-Add-CakeStep -Name "Build All" -Action {  Build-Dotnet -All  }
-Add-CakeStep -Name "Publish to nuget.org" -Action { 
-	Add-ApiToken "https://nuget.org" "oy2gob3hlzwww46y5ggq5gp6mwyx5cwebpodezn7zbhz4i"
-	Publish-Nuget -SourceUrl "https://nuget.org" 
-}

ccc64e5a9c1a6e98d675cb66f46401be9484b485
Rafał Maciąg <rafal.maciag@modelingevolution.com>
2024-04-17 16:38:21 +0200

    Cylindrical point going public

---
+++ Sources/ModelingEvolution.Drawing.Tests/PolarPointTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+using PolarPointF=ModelingEvolution.Drawing.PolarPoint<float>;
+using VectorF = ModelingEvolution.Drawing.Vector<float>;
+using PointF = ModelingEvolution.Drawing.Point<float>;
+using CylindricalPointF = ModelingEvolution.Drawing.CylindricalPoint<float>;
+
+namespace ModelingEvolution.Drawing.Tests
+{
+    public class PolarPointTests
+    {
+        private readonly ITestOutputHelper testOutputHelper;
+
+        public PolarPointTests(ITestOutputHelper testOutputHelper)
+        {
+            this.testOutputHelper = testOutputHelper;
+        }
+
+        class LidarDataSource
+        {
+            public IEnumerable<PolarPointF> Read()
+            {
+                yield return new PolarPointF(Degree<float>.Create(30), 10);
+                yield return new PolarPointF(Degree<float>.Create(45), 12);
+                yield return new PolarPointF(Degree<float>.Create(60), 18);
+            }
+        }
+
+        [Fact]
+        public void CalculatePointFromLidar()
+        {
+            LidarDataSource src = new LidarDataSource();
+
+            IEnumerable<PolarPointF> points = src.Read();
+
+            Vector3 v = new Vector3(0,0,5f);
+            TimeSpanF dt = TimeSpan.FromSeconds(1f/10); // 10Hz
+            Vector3 start = Vector3.Zero;
+
+            start += v * dt;
+            foreach (var i in points)
+            {
+                CylindricalPointF cp = i;
+                var xyPoint = start + cp;
+                testOutputHelper.WriteLine(xyPoint.ToString());
+            }
+        }
+    }
+}
--- Sources/ModelingEvolution.Drawing/PolarPoint.cs
+++ Sources/ModelingEvolution.Drawing/PolarPoint.cs
@@ -3,6 +3,36 @@ using System.Numerics;
 
 namespace ModelingEvolution.Drawing;
 
+[DebuggerDisplay("[{RadialDistance},{Angle},{Z}]")]
+public readonly record struct CylindricalPoint<T>
+    where T : IFloatingPointIeee754<T>
+{
+    public Radian<T> Angle { get; }
+    public T RadialDistance { get; }
+    public T Z { get; }
+    public CylindricalPoint(Radian<T> angle, T r, T z)
+    {
+        Angle = angle;
+        RadialDistance = r;
+        Z = z;
+    }
+
+    public static implicit operator CylindricalPoint<T>(PolarPoint<T> point)
+    {
+        return new CylindricalPoint<T>(point.Angle, point.Radius, T.Zero);
+    }
+    public static implicit operator Vector3(CylindricalPoint<T> point)
+    {
+        T x = point.RadialDistance * T.Cos((T)point.Angle);
+        T y = point.RadialDistance * T.Sin((T)point.Angle);
+        return new Vector3(float.CreateTruncating(x), float.CreateTruncating(y), float.CreateTruncating(point.Z));
+    }
+    public override string ToString()
+    {
+        return $"[{RadialDistance},{Angle},{Z}]";
+    }
+}
+
 [DebuggerDisplay("[{Angle},{Radius}]")]
 public readonly record struct PolarPoint<T>
     where T : IFloatingPointIeee754<T>
@@ -43,5 +73,10 @@ public readonly record struct PolarPoint<T>
         T x = point.Radius * T.Cos((T)point.Angle);
         T y = point.Radius * T.Sin((T)point.Angle);
         return new Point<T>(T.CreateTruncating(x), T.CreateTruncating(y));
+    }
+
+    public override string ToString()
+    {
+        return $"[{Radius},{Angle}]";
     }
 }
